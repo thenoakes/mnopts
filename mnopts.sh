f6ad0b42e3df173d32cdc3626c92063c6c59d8bf
Matt Noakes <mattnoakes@me.com>
2020-04-05 22:14:51 +0100

    Now works via sourcing and environment variables

--- mnopts.sh
+++ mnopts.sh
@@ -18,86 +18,82 @@ checkDuplicate () {
   return 1
 }
 
-# Parse the first argument as an array
-allOptions=($1)
+# Only act if MNOPTS are supplied
+if [ ! -z "$MNOPTS" ]; then
 
-# Set the usage statement from the second argument
-usage="Usage: $2"
+    # BEGIN PARSING
 
-# Drop the above 2 positional arguments
-shift 2
+    allOptions=($MNOPTS)
 
-# Build a list of the valid short-form options and set up the automatic bool variables
-allLong=()
-allShort=()
-for option in ${allOptions[@]}
-do
-    IFS='-' read -ra optionPair <<< "$option"
+    # Set the usage statement if MNOPTS_USAGE is supplied
+    if [ ! -z "$MNOPTS_USAGE" ]; then
+        usage="Usage: $MNOPTS_USAGE"
+    else
+        usage="Incorrect usage"
+    fi
 
-    longOpt="${optionPair[0]}"
-    shortOpt="${optionPair[1]}"
-
-    # If either the long or short options alreadt exist, bail out
-    checkDuplicate "$longOpt" "${allLong[@]}" && exit 1
-    checkDuplicate "$shortOpt" "${allShort[@]}" && exit 1
-
-    # Add the long & short options to a list, and set the default value for the variable
-    allLong+=("$longOpt")
-    allShort+=("$shortOpt")
-    declare opt_${shortOpt}=false
-done
-
-shortOptions=$(printf "%s" "${allShort[@]}")
-
-# Match the arguments passed from the calling script against the options
-for arg in "$@"
-do
-    shift
-    optionMatched=false
-
-    # Check the passed option against all registered options
+    # Build a list of the valid short-form options and set up the automatic bool variables
+    allLong=()
+    allShort=()
     for option in ${allOptions[@]}
     do
-
-        # Get the 'long form'
         IFS='-' read -ra optionPair <<< "$option"
 
-        # Use it to set the 'short' form
-        if [ "$arg" == "--${optionPair[0]}" ]
+        longOpt="${optionPair[0]}"
+        shortOpt="${optionPair[1]}"
+
+        # If either the long or short options alreadt exist, bail out
+        checkDuplicate "$longOpt" "${allLong[@]}" && exit 1
+        checkDuplicate "$shortOpt" "${allShort[@]}" && exit 1
+
+        # Add the long & short options to a list, and set the default value for the variable
+        allLong+=("$longOpt")
+        allShort+=("$shortOpt")
+        declare opt_${shortOpt}=false
+    done
+
+    shortOptions=$(printf "%s" "${allShort[@]}")
+
+    # Match the arguments passed from the calling script against the options
+    for arg in "$@"
+    do
+        shift
+        optionMatched=false
+
+        # Check the passed option against all registered options
+        for option in ${allOptions[@]}
+        do
+
+            # Get the 'long form'
+            IFS='-' read -ra optionPair <<< "$option"
+
+            # Use it to set the 'short' form
+            if [ "$arg" == "--${optionPair[0]}" ]
+            then
+                optionMatched=true
+                set -- "$@" "-${optionPair[1]}"
+            fi
+        done
+
+        # If not in the registered list, echo usage and exit
+        if [ "$optionMatched" == "false" ]
         then
-            optionMatched=true
-            set -- "$@" "-${optionPair[1]}"
+            set -- "$@" "$arg"
         fi
     done
 
-    # If not in the registered list, echo usage and exit
-    if [ "$optionMatched" == "false" ]
-    then
-        set -- "$@" "$arg"
-    fi
-done
+    # Use standard getopts setup now that long-form options have been converted to short
+    OPTIND=1
+    while getopts "$shortOptions" opt 2> /dev/null
+    do
+        case "$opt" in
+            "?")    echo "$usage" >&2 && exit 1 ;; 
+            "h")    echo "$usage" && exit 0 ;; 
+            *)      declare opt_${opt}=true ;;
+        esac
+    done
+    shift $(expr $OPTIND - 1)
 
-# Use standard getopts setup now that long-form options have been converted to short
-OPTIND=1
-while getopts "$shortOptions" opt 2> /dev/null
-do
-    case "$opt" in
-        "?")    echo "$usage" >&2 && exit 1 ;; 
-        "h")    echo "$usage" && exit 0 ;; 
-        *)      declare opt_${opt}=true ;;
-    esac
-done
-shift $(expr $OPTIND - 1)
+    # END PARSING
 
-# Build up the ouput which involves a list of variable values and the original variables
-output=""
-for option in ${allOptions[@]}
-do
-    IFS='-' read -ra optionPair <<< "$option"
-    varName="opt_${optionPair[1]}"
-    output="${output}opt_${optionPair[1]}=${!varName}&&" 
-done
-output="${output}:" 
-
-# Echo the output so that the calling script can use it
-echo "$output $@"
+fi
--- test.sh
+++ test.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Calling mnopts returns the original arguments plus the parsed values as the first value
-set -- $(./mnopts.sh "long-l short-s another-a" "test.sh [-l|--long] [-s|--short]" "$@")
-
-[[ -z "$1" ]] && exit 1
-
-# Set the parsed variables and remove them
-eval "$1"
-shift
+MNOPTS="long-l short-s another-a" MNOPTS_USAGE="test.sh [-l|--long] [-s|--short] [-a|--another]"
+source ./mnopts.sh
 
 # Automatic parsed boolean arguments of the form opt_x where x is the short option
 echo "opt_l=$opt_l"
 echo "opt_s=$opt_s"
+echo "opt_a=$opt_a"
 
 echo "$1"
 echo "$2"
